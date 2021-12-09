7e2e620bacaae7aedbb92fdf67b5a80c43719543
Dalsi Patel <patel.dal@northeastern.edu>
2021-12-09 17:07:23 -0500

    Changes related to Delivery Man

--- SARS-CoV-2 Resource Management System/src/Business/DeliveryMan/DeliveryManDirectory.java
+++ SARS-CoV-2 Resource Management System/src/Business/DeliveryMan/DeliveryManDirectory.java
@@ -5,10 +5,53 @@
  */
 package Business.DeliveryMan;
 
+import java.util.HashMap;
+
 /**
  *
  * @author harold
  */
 public class DeliveryManDirectory {
+    
+     private HashMap<String, DeliveryMan> deliveryManList;
+
+    public DeliveryManDirectory() {
+        this.deliveryManList=new HashMap<>();
+    }
+    
+    //Find delivery man using deliveryman id
+    public DeliveryMan findDeliveryMan(String delID){
+        DeliveryMan dm = this.deliveryManList.get(delID);
+        if(dm==null)
+            return null;
+        return dm;
+    }
+    
+    public DeliveryMan findDeliveryManByUserName(String userName){
+        
+        for(String s: this.deliveryManList.keySet()){
+            if(this.deliveryManList.get(s).getDeliveryManAccount().getUsername().equals(userName)){
+                return this.deliveryManList.get(s);
+            }
+        }
+        return null;
+    }
+
+    public HashMap<String, DeliveryMan> getDeliveryManList() {
+        return deliveryManList;
+    }
+    
+    //Creating new delivery man
+    public DeliveryMan newDeliveryMan(){
+        DeliveryMan dm = new DeliveryMan();
+        this.deliveryManList.put(dm.getDelId(), dm);
+        return this.deliveryManList.get(dm.getDelId());
+    }
+    
+    //Removing delivery man
+    public void removeDeliveryMan(String delID){
+        this.deliveryManList.remove(this.deliveryManList.get(delID));
+    }
+    
     
 }
