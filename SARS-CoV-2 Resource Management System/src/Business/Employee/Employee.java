e5e971d6c3b9e2201d68313c05d390e1b2c5b8fb
Adrenaltrack93 <vise.s@northeastern.edu>
2021-12-11 18:40:40 -0500

    Changes related to Food Delivery

--- SARS-CoV-2 Resource Management System/src/Business/Employee/Employee.java
+++ SARS-CoV-2 Resource Management System/src/Business/Employee/Employee.java
@@ -4,29 +4,41 @@
  */
 package Business.Employee;
 
+import Business.UserAccount.UserAccount;
+
 /**
  *
  * @author raunak
  */
 public class Employee {
     
+    private static int temp=0;
+    
     private String name;
-    private int id;
+    private String empId;
     private static int count = 1;
+    private UserAccount userAccount;
 
     public Employee() {
-        id = count;
-        count++;
+        temp++;
+        this.empId= "E"+String.valueOf(temp);
     }
-
-    public int getId() {
-        return id;
+    
+    public String getEmpId() {
+        return empId;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+    
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
 
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
     
     public String getName() {
         return name;
@@ -36,6 +48,5 @@ public class Employee {
     public String toString() {
         return name;
     }
-    
     
 }
