cd08c93263ca9b7508743eca0a5fca1afbc837c2
Dalsi Patel <patel.dal@northeastern.edu>
2021-12-11 17:41:36 -0500

    Changes for Food Delivery

--- SARS-CoV-2 Resource Management System/src/Business/Role/CustomerRole.java
+++ SARS-CoV-2 Resource Management System/src/Business/Role/CustomerRole.java
@@ -6,7 +6,6 @@ package Business.Role;
 
 import Business.DB4OUtil.DB4OUtil;
 import Business.EcoSystem;
-import Business.Enterprise.Enterprise;
 
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
@@ -20,7 +19,6 @@ import javax.swing.JPanel;
 public class CustomerRole extends Role{
 
     
-     
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business,DB4OUtil dB4OUtil) {
         return new CustomerAreaJPanel(userProcessContainer, account, business, dB4OUtil);
     }
