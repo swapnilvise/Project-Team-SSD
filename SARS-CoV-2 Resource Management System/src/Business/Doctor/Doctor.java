59aeb0c26d0368650a0781c7f98b10fd1fedc7bf
Adrenaltrack93 <vise.s@northeastern.edu>
2021-12-15 04:06:05 -0500

    Changes Related to Doctor and Lab

---
+++ SARS-CoV-2 Resource Management System/src/Business/Doctor/Doctor.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Doctor;
+
+import Business.Student.Student;
+
+/**
+ *
+ * @author swapn
+ */
+public class Doctor {
+    private String DoctorLastName;
+    private String DoctorFirstName;
+    private String DoctorID;
+    private UserAccount doctorAccount;
+    private static int count = 0;
+    
+    public Doctor(){
+        count++;
+        this.DoctorID="D"+String.valueOf(count);
+    }
+
+    public String getDoctorLastName() {
+        return DoctorLastName;
+    }
+
+    public void setDoctorLastName(String DoctorLastName) {
+        this.DoctorLastName = DoctorLastName;
+    }
+
+    public String getDoctorFirstName() {
+        return DoctorFirstName;
+    }
+
+    public void setDoctorFirstName(String DoctorFirstName) {
+        this.DoctorFirstName = DoctorFirstName;
+    }
+
+    public String getDoctorID() {
+        return DoctorID;
+    }
+
+    public void setDoctorID(String DoctorID) {
+        this.DoctorID = DoctorID;
+    }
+    
+    @Override
+    public String toString(){
+        return DoctorLastName;
+    }
+    
+}
